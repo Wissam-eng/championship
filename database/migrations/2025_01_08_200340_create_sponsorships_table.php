a2027d831927c115ae2f739cc666ec1959925029
wesam <wissamomran8@gmail.com>
2025-01-15 19:34:26 +0200

    gui commit

--- database/migrations/2025_01_08_200340_create_sponsorships_table.php
+++ database/migrations/2025_01_08_200340_create_sponsorships_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('image')->nullable();
             $table->string('title');
+            $table->string('name');
             $table->text('description');
             $table->text('link')->nullable();
             $table->timestamps();
